c1da59a41fb6361a541052a69a84e2aa4fc004f8
Saideep-123 <tuttepus@gmail.com>
2026-01-27 19:02:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Categories from "./components/Categories";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
 import CartDrawer from "./components/CartDrawer";
-import { useState } from "react";
 
 export default function HomePage() {
   const [activeCategory, setActiveCategory] = useState("All");
@@ -16,8 +16,8 @@ export default function HomePage() {
 
       <main id="home">
         <Categories
-          activeCategory={activeCategory}
-          onChange={setActiveCategory}
+          active={activeCategory}
+          setActive={setActiveCategory}
         />
 
         <Products activeCategory={activeCategory} />
