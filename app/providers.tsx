e04c4733276ca391937d088704dd6ce0b51957d9
Saideep-123 <tuttepus@gmail.com>
2026-01-28 00:29:44 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { CartProvider } from "./components/CartContext";
-import { AuthProvider } from "./components/AuthContext"; // adjust path/name if yours differs
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthProvider>
-      <CartProvider>{children}</CartProvider>
-    </AuthProvider>
-  );
+  return <CartProvider>{children}</CartProvider>;
 }
