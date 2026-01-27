e90da1f6572b0efb68e8700882ae121bb221f361
Saideep-123 <tuttepus@gmail.com>
2026-01-28 00:20:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,5 @@
-"use client";
+import HomeClient from "./HomeClient";
 
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Categories from "./components/Categories";
-import Products from "./components/Products";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import CartDrawer from "./components/CartDrawer";
-
-export default function Home() {
-  const [activeCategory, setActiveCategory] = useState("All");
-  const [searchQuery, setSearchQuery] = useState("");
-
-  return (
-    <>
-      <Navbar />
-      <CartDrawer />
-      <main>
-        <Hero />
-        <Categories
-          active={activeCategory}
-          setActive={setActiveCategory}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
-        <Products activeCategory={activeCategory} searchQuery={searchQuery} />
-        <About />
-        <Footer />
-      </main>
-    </>
-  );
+export default function Page() {
+  return <HomeClient />;
 }
